7b46229bc43b10f1c07df7dd74d6107c4e9217db
benjaminli7 <nicky01.bl@gmail.com>
2021-06-30 16:16:30 +0200

    first commit

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 window.addEventListener("load", function (event) {
-    document.querySelector("body").classList.remove("preload");
+  document.querySelector("body").classList.remove("preload");
 });
 
-const hamburger = document.getElementById('icon-hamburger');
-const icon_close = document.getElementById('icon-close');
-const menu = document.getElementById('nav-menu');
-const hero = document.getElementById('hero');
-const hero_title = document.getElementById('hero-title')
+const hamburger = document.getElementById("icon-hamburger");
+const icon_close = document.getElementById("icon-close");
+const menu = document.getElementById("nav-menu");
+const hero = document.getElementById("hero");
+const hero_title = document.getElementById("hero-title");
 
-hamburger.addEventListener('click', () => {
-    hamburger.style.display = 'none';
-    icon_close.style.display = 'block';
-    menu.style.display = 'block';
-    hero.style.backgroundImage = 'none'
-    hero.style.backgroundColor = 'black';
-    hero_title.style.display = 'none';
-    document.body.classList.add('fixed');
-})
+hamburger.addEventListener("click", () => {
+  hamburger.style.display = "none";
+  icon_close.style.display = "block";
+  menu.style.display = "block";
+  hero.style.backgroundImage = "none";
+  hero.style.backgroundColor = "black";
+  hero_title.style.display = "none";
+  document.body.classList.add("fixed");
+});
 
-icon_close.addEventListener('click', () => {
-    hamburger.style.display = 'block';
-    icon_close.style.display = 'none';
-    menu.style.display = 'none';
-    hero.style.backgroundImage = "url('images/mobile/image-hero.jpg')";
-    hero_title.style.display = 'block';
-    document.body.classList.remove('fixed');
-})
+icon_close.addEventListener("click", () => {
+  hamburger.style.display = "block";
+  icon_close.style.display = "none";
+  menu.style.display = "none";
+  hero.style.backgroundImage = "url('images/mobile/image-hero.jpg')";
+  hero_title.style.display = "block";
+  document.body.classList.remove("fixed");
+});
